3c193a6dae8692e676c0170cd8d69a6a27e6e220
Ninganna <arunningu@gmail.com>
2024-04-04 13:01:01 +0530

    last commit

--- server/src/config/database.js
+++ server/src/config/database.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 async function connectDB() {
   try {
-    await mongoose.connect(process.env.MONGODB_URI);
+    await mongoose.connect('mongodb://localhost:27017');
     console.log("Connected to MongoDB Atlas");
   } catch (error) {
     console.error("Error connecting to MongoDB Atlas:", error);
